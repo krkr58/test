9a56cb5717ce93f578dc80b872be6cec8dd60995
krkr58 <k-kazu08@rakuten.jp>
2019-05-30 17:51:16 +0900

    0530

---
+++ template/src/com/internousdev/template/util/DBConnector.java
@@ -0,0 +1,27 @@
+package com.internousdev.template.util;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DBConnector {
+	private static String driverName="com.mysql.jdbc.Driver"; //ドライバー名
+
+	private static String url="jdbc:mysql://localhost/ecsite"; //接続先データーベース
+
+	private static String user="root"; //ユーザー名
+	private static String password="mysql"; //パスワード
+
+	public Connection getConnection(){
+		Connection con=null;
+
+		try{
+			Class.forName(driverName);
+			con=DriverManager.getConnection(url,user,password);
+		}catch (ClassNotFoundException e){
+			e.printStackTrace();
+		}catch (SQLException e){
+			e.printStackTrace();
+		}
+		return con;
+	}
+}
